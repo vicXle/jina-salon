eda822ff4a1823548a9fc74cbe73c395b9532b61
vicXle <vkle1@asu.edu>
2023-11-27 08:35:50 -0700

    fine tuning

--- script.js
+++
@@ -1,7 +0,0 @@
-var nav = document.getElementById('nav'),
-body = document.body;
-
-nav.addEventListener('click', function(e) {
-    body.className = body.className? '' : 'with_nav';
-    e.preventDefault();
-});
